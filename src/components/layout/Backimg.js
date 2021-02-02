4ff047f182a8669823a422d987a080f271ae9285
RafaelAlvesSantos <rafaelalvesmgs@gmail.com>
2021-02-02 16:10:41 -0300

    added the opening background image and text

---
+++ src/components/layout/Backimg.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Middle from "./Middletext";
+
+const Backimg = () => {
+  return (
+    <div className="backimg">
+      <img
+        src="https://image.shutterstock.com/image-photo/view-financial-district-manhattan-new-260nw-429089002.jpg"
+        alt=""
+      />
+      <div className="after"></div>
+      <Middle />
+    </div>
+  );
+};
+
+export default Backimg;
---
+++ src/components/layout/Middletext.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Middletext = () => {
+  return (
+    <div className="centro">
+      <p className="textoMeio">CONSIST.CSO</p>
+    </div>
+  );
+};
+
+export default Middletext;
